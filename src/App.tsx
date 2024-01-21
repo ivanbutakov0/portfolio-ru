465786f3634b05d4c476225a1b6884ac8c9e3fb7
Ivan Butakov <ivanbutakov000@gmail.com>
2024-01-21 20:03:02 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Projects from './pages/Projects/Projects'
+
+function App() {
+	return (
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/projects' element={<Projects />} />
+		</Routes>
+	)
+}
+
+export default App
